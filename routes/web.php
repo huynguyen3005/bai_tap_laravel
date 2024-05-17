8caad2f67508d83b5526ad1d381a24086ae9fbf4
Nguyễn Văn Huy <107173480+huynguyen3005@users.noreply.github.com>
2024-05-17 11:46:45 +0700

    delete unused code and clean code

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,7 @@ Route::prefix('admin')->middleware(CheckRole::class)->group(function(){
 
     Route::middleware(['checkAdminRole:Editor'])->group(function () {
         Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/{user}', function(){
-            return '1';
-        })->name('users.destroy');
+        Route::delete('/users/{user}',[UserController::class, 'destroy'])->name('users.destroy');
     });
 
     Route::middleware(['checkAdminRole:Viewer'])->group(function () {
